216a29b48bd2ffc3cab6441702c64f2a9c4a5d03
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2026-03-03 10:10:31 -0300

    Atividade termometro - LIP I

--- Atividade_termômetro/Questao-6.c
+++ Atividade_termômetro/Questao-6.c
@@ -11,8 +11,11 @@ int ehprimo(int n){
     }
     if(n == 2){
         return 1;
+    }
+    if(n%2==0){
+        return 0;
     }else{
-        for(int i = 2; i<=n; i++){
+        for(int i = 3; i*i<=n; i+=2){
             if(n%i == 0){
                 return 0;
             }else{
@@ -22,7 +25,6 @@ int ehprimo(int n){
     }
     return 1;
 }
-
 int main(){
     int n;
     printf("Digite um numero primo:\n");
